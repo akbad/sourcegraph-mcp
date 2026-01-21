ec71db1864f8b1f6bb6af1c710b82dd16e61f2d2
akbad <dakbarza@uwaterloo.ca>
2026-01-20 19:26:03 -0500

    Improve inline docs for results' data models defs

--- src/backends/models.py
+++ src/backends/models.py
@@ -1,16 +1,23 @@
+"""
+This file contains definitions of data models to be used in search results.
+"""
+
 from dataclasses import dataclass
 from typing import List
 
 
+# Note the @dataclass decorator auto-generates necessary but trivial utility functions for 
+#   simple data container classes like these.
+
 @dataclass
 class Match:
-    line_number: int
-    text: str
+    line_number: int  # which line matched
+    text: str         # content of the matched line
 
 
 @dataclass
 class FormattedResult:
-    filename: str
-    repository: str
-    matches: List[Match]
-    url: str
+    filename: str         # filename containing match
+    repository: str       # repo containing the file
+    matches: List[Match]  # all matching lines in the file
+    url: str              # Sourcegraph URL at which file can be viewed
